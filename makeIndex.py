0cfc15f78f248366d7dc02e0512075b6bf68a9ff
Andrew Levin <andrewlevingit@hotmail.com>
2023-10-01 21:46:53 +0300

    57

--- makeIndex.py
+++ makeIndex.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-
+import json
 import pathlib
+import pprint
+from string import Template
 
 import telegram
 import yaml
@@ -13,10 +15,9 @@ from utils import read_file, write_file
 from markdownify import markdownify as md
 
 def text_Preprocessing(text_html):
-    print('🦜', 'Preprocessing text: ')
-    print(text_html)
-    print('==============')
-
+    #print('🦜', 'Preprocessing text: ')
+    #print(text_html)
+    #print('==============')
 
     for url in URLExtract().find_urls(text_html):
         text_html = text_html.replace(url, f'<a href="{url}">{url}</a>')
@@ -24,22 +25,84 @@ def text_Preprocessing(text_html):
     #text_html = md(text_html)
     text_html = text_html.replace('\n', '<br>\n')
 
-    #print('🦜🦜')
-    #print(text_html)
-    #print('==============')
-
-    #print()
-    #print()
-
     return text_html
 
 
+def renderForwardPostContent(message: telegram.Message, data: dict):
+    print('🛍 Forward')
+    print('Message: ')
+    print(message)
+    print('===========')
+    print()
+    print()
+
+    contents = []
+
+    forw_chat = ''
+    if hasattr(message, 'forward_from_chat'):
+        if hasattr(message.forward_from_chat, 'title'):
+            forw_chat += f'<strong>{message.forward_from_chat.title}</strong>'
+
+        if hasattr(message.forward_from_chat, 'username'):
+            forw_chat += f'<small>({message.forward_from_chat.username})</small>'
+
+        if hasattr(message.forward_from_chat, 'id'):
+            forw_chat += f' <small>[{message.forward_from_chat.id}]</small>'
+
+    contents.append(f'<h5>{forw_chat}</h5>')
+
+    if hasattr(message, 'forward_date'):
+        contents.append(f'<p>{message.forward_date}</p>')
+
+    contents.append(f'TEXT')
+    contents.append('==========')
+    contents.append(text_Preprocessing(data.get('text_html')))
+
+    content = '<br>'.join(contents)
+
+    forw_templ = '''
+    <div class="border border-primary ml-2 pl-3" 
+        style="
+            border-right: none!important;
+            margin-left: .5rem;
+            padding-left: 1rem;
+            border-bottom: none!important;
+            border-top: none!important;
+            border-width: 1px!important;">
+            $content
+            </div>
+            '''
+
+    content = Template(forw_templ).substitute(content=content)
+
+    return content
+
+    template = Environment(loader=FileSystemLoader("templates")).get_template("forwardPostContent.html")
+    return template.render(
+        {'title': f'Post {message.message_id}',
+         'date': message.date.__str__(),
+         'content': content,
+         })
+
 def renderPostCard(message: telegram.Message, data: dict):
+    #print('🖼 ', message.message_id)
+    #print(yaml.dump(message, default_flow_style=False))
+    #print('========')
+    #print()
+    #print()
+    #print()
+
+    content = ''
+    if hasattr(message, 'forward_date'):
+        content = renderForwardPostContent(message, data)
+    else:
+        content = text_Preprocessing(data.get('text_html'))
+
     template = Environment(loader=FileSystemLoader("templates")).get_template("post.html")
     return template.render(
         {'title': f'Post {message.message_id}',
          'date': message.date.__str__(),
-         'content': text_Preprocessing(data.get('text_html')),
+         'content': content,
          })
 
 
